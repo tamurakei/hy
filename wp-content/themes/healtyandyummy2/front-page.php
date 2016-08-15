aad1d21660e0180e459ce6564026162fdcc160c7
Match <matchkoba@gmail.com>
2016-08-15 09:14:58 +0900

    no message

--- wp-content/themes/healtyandyummy2/front-page.php
+++ wp-content/themes/healtyandyummy2/front-page.php
@@ -30,10 +30,10 @@
 					<?php endif; ?>	
 					</div>
 					
-					
+					<div class="txt bordertop"><?php the_field('text'); ?></div>
 					<div class="copy"><?php the_field('copy'); ?></div>
 					<div class="policy bordertop"><?php the_field('policy'); ?></div>
-					<div class="txt bordertop"><?php the_field('text'); ?></div>
+					
 					<div class="material bordertop"><?php the_field('material'); ?></div>
 				</div>
 
