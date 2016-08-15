d6326837a5207b1e9981a63ab8695d3e89f64b73
田村啓 <tamura@shikumilab.jp>
2016-08-15 23:54:29 +0900

    カード情報登録画面の日本語化

--- wp-content/plugins/omise-woocommerce-master/includes/templates/omise-cc-form.php
+++ wp-content/plugins/omise-woocommerce-master/includes/templates/omise-cc-form.php
@@ -1,30 +1,30 @@
 <p class="form-row form-row-wide omise-required-field">
-	<label for="omise_card_name">Name <span class="required">*</span></label>
+	<label for="omise_card_name">カード名義 <span class="required">*</span></label>
 	<input id="omise_card_name" class="input-text" type="text"
 		maxlength="255" autocomplete="off" placeholder="Name"
 		name="omise_card_name">
 </p>
 <p class="form-row form-row-wide omise-required-field">
-	<label for="omise_card_number">Card Number <span class="required">*</span></label>
+	<label for="omise_card_number">カード番号 <span class="required">*</span></label>
 	<input id="omise_card_number" class="input-text" type="text"
-		maxlength="20" autocomplete="off" placeholder="Card number"
+		maxlength="20" autocomplete="off" placeholder="0000000000000000"
 		name="omise_card_number">
 </p>
 <p class="form-row form-row-first omise-required-field">
-	<label for="omise_card_expiration_month">Expiration month <span
+	<label for="omise_card_expiration_month">有効月 <span
 		class="required">*</span></label>
 	<input id="omise_card_expiration_month" class="input-text" type="text"
 		autocomplete="off" placeholder="MM" name="omise_card_expiration_month">
 </p>
 <p class="form-row form-row-last omise-required-field">
-	<label for="omise_card_expiration_year">Expiration year <span
+	<label for="omise_card_expiration_year">有効年 <span
 		class="required">*</span></label>
 	<input id="omise_card_expiration_year" class="input-text" type="text"
 		autocomplete="off" placeholder="YYYY"
 		name="omise_card_expiration_year">
 </p>
 <p class="form-row form-row-first omise-required-field">
-	<label for="omise_card_security_code">Security Code <span
+	<label for="omise_card_security_code">セキュリティコード <span
 		class="required">*</span></label>
 	<input id="omise_card_security_code"
 		class="input-text" type="password" autocomplete="off"
