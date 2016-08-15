7c631ba9034a47b0aa4828f72eec6061b014b476
田村啓 <tamura@shikumilab.jp>
2016-08-15 10:53:08 +0900

    single-product.php を WooCommerce のものに

--- wp-content/themes/healtyandyummy2/single-product.php
+++ wp-content/themes/healtyandyummy2/single-product.php
@@ -1,46 +1,59 @@
-<?php get_header(); ?>
-	
-		<?php if(have_posts()):while(have_posts()):the_post();?>
-							
-		<div class="articleWrap">
-			<div class="article">
-				<?php /*
-				<div class="articleTitle">
-				<h2><?php the_field('name'); ?></h2>
-				</div>
-				*/ ?>
-				
-				<div class="articleTxt">
-					<?php $image = get_field('image');?>
-					<a href="<?php echo $image['url']; ?>" class="cboxElement"><img class="p_img alignnone  wp-image-45 colorbox-<?php echo $image['id']; ?>" src="<?php echo $image['url']; ?>"></a>
-					<div class="description">
-					
-					<div class="itembox">
-					<h2 class="articleTitle"><?php the_field('name'); ?></h2>
-					<p class="price"><?php the_field('price'); ?> - (tax incl.) &nbsp;&nbsp;</p>
-					<?php if(get_field('button')): ?>
-						<?php if(get_field('button') == "visible"): ?>
-					<div class="buybutton">
-						<a href="http://store.healthy-yummy.com/items/2301472" target="_blank">ADD TO BAG</a>
-					</div>
-					
-	
-						<?php endif; ?>
-					<?php endif; ?>	
-					</div>
-					
-					
-					<div class="copy"><?php the_field('copy'); ?></div>
-					<div class="policy bordertop"><?php the_field('policy'); ?></div>
-					<div class="txt bordertop"><?php the_field('text'); ?></div>
-					<div class="material bordertop"><?php the_field('material'); ?></div>
-				</div>
+<?php
+/**
+ * The Template for displaying all single products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
 
-			</div>
-		</div>
-		<div class="articleFadeU"></div>
-		<div class="articleFadeB"></div>
-		<div class="clearfix"></div>
-		</div>
-		<?php endwhile; endif; ?>
-<?php get_footer(); ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header( 'shop' ); ?>
+
+	<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php wc_get_template_part( 'content', 'single-product' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+	<?php
+		/**
+		 * woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
+
+	<?php
+		/**
+		 * woocommerce_sidebar hook.
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action( 'woocommerce_sidebar' );
+	?>
+
+<?php get_footer( 'shop' ); ?>
