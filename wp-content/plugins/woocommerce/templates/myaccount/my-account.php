3473bb2fe433a59c63c17642e8e334e2868e6b43
田村啓 <tamura@shikumilab.jp>
2016-08-16 23:08:49 +0900

    my-account.php の修正

--- wp-content/plugins/woocommerce/templates/myaccount/my-account.php
+++ wp-content/plugins/woocommerce/templates/myaccount/my-account.php
@@ -37,23 +37,11 @@ do_action( 'woocommerce_account_navigation' ); ?>
 		do_action( 'woocommerce_account_content' );
 	?>
 
-<p class="myaccount_user">
-	<?php
-	printf(
-		__( 'Hello <strong>%1$s</strong> (not %1$s? <a href="%2$s">Sign out</a>).', 'woocommerce' ) . ' ',
-		$current_user->display_name,
-		wc_get_endpoint_url( 'customer-logout', '', wc_get_page_permalink( 'myaccount' ) )
-	);
+<h3>登録情報<h3>
+<?php wc_get_template( 'myaccount/form-edit-account.php' ); ?>
 
-	printf( __( 'From your account dashboard you can view your recent orders, manage your shipping and billing addresses and <a href="%s">edit your password and account details</a>.', 'woocommerce' ),
-		wc_customer_edit_account_url()
-	);
-	?>
-</p>
-
-<?php wc_get_template( 'myaccount/my-orders.php', array( 'order_count' => $order_count ) ); ?>
-
+<h3>住所<h3>
 <?php wc_get_template( 'myaccount/my-address.php' ); ?>
 
-<?php do_action( 'woocommerce_after_my_account' ); ?>
+</div>
 
