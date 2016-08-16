e438ae5063ac6287bb3b0068f73eb667c03d584c
Match <matchkoba@gmail.com>
2016-08-16 10:26:27 +0900

    no message

--- wp-content/plugins/woocommerce-pip/woocommerce-pip-template/template-body.php
+++ wp-content/plugins/woocommerce-pip/woocommerce-pip-template/template-body.php
@@ -8,11 +8,12 @@
 					</div>
 					<div style="float: left; width: 49%;"  class="printhead">
 						 <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt='Healthy&yummy' width="300" class="logo">
-						<?php if ($action == 'print_invoice') { ?>
+						 <h3>RECEIPT</h3>
+						<!--<?php if ($action == 'print_invoice') { ?>
 						<h3><?php _e('Invoice', 'woocommerce-pip'); ?> (<?php echo woocommerce_pip_invoice_number($order->id); ?>)</h3>
 						<?php } else { ?>
 						<h3><?php _e('Packing list', 'woocommerce-pip'); ?></h3>
-						<?php } ?>
+						<?php } ?>-->
 						
 					</div>
 					
@@ -50,7 +51,7 @@
 						
 							<p><?php echo woocommerce_pip_print_company_extra(); ?></p>
 							<h3><?php echo woocommerce_pip_print_company_name(); ?></h3>
-							<h3>提坂 清士</h3>
+							
 						<!-- End Company -->
 						<div style="clear:both;"></div>
 						<!--Shipping<h3><?php _e('Shipping address', 'woocommerce-pip'); ?></h3>
@@ -99,56 +100,56 @@
 				<thead>
 					<tr class="desc">
 					  <th scope="col" style="text-align:left; width: 15%;"><?php _e('SKU', 'woocommerce-pip'); ?></th>
-						<th scope="col" style="text-align:left; width: 45%;"><?php _e('Product', 'woocommerce-pip'); ?></th>
+						<th scope="col" style="text-align:left; width: 60%;"><?php _e('Product', 'woocommerce-pip'); ?></th>
 						<th scope="col" style="text-align:left; width: 10%;"><?php _e('Quantity', 'woocommerce-pip'); ?></th>
-						<th scope="col" style="text-align:left; width: 30%;"><?php _e('Price', 'woocommerce-pip'); ?></th>
+						<th scope="col" style="text-align:left; width: 15%;"><?php _e('Price', 'woocommerce-pip'); ?></th>
 					</tr>
 				</thead>
 				<tfoot>
 					<tr class="desc" >
 					  <th colspan="2" style="text-align:left; padding-top: 34px !important;">&nbsp;</th>
 						<th class="info"  scope="row" style="text-align:left; padding-top: 34px !important;"><?php _e('Subtotal:', 'woocommerce-pip'); ?></th>
-						<td style="text-align:left;padding-top: 34px !important;"><?php echo $order->get_subtotal_to_display(); ?></td>
+						<td style="text-align:right;padding-top: 34px !important;"><?php echo $order->get_subtotal_to_display(); ?></td>
 					</tr>
 					<?php if (get_option('woocommerce_calc_shipping')=='yes') : ?><tr class="desc" >
 					  <th colspan="2" style="text-align:left; padding-top: 12px;">&nbsp;</th>
 						<th class="info"  scope="row" style="text-align:left;"><?php _e('Shipping:', 'woocommerce-pip'); ?></th>
-						<td style="text-align:left;"><?php echo $order->get_shipping_to_display(); ?></td>
+						<td style="text-align:right;"><?php echo $order->get_shipping_to_display(); ?></td>
 					</tr><?php endif; ?>
 					<?php echo woocommerce_pip_order_fees($order); ?>
 					<?php if ($order->cart_discount > 0) : ?><tr class="desc" >
 					  <th colspan="2" style="text-align:left; padding-top: 12px;">&nbsp;</th>
 						<th class="info" scope="row" style="text-align:left;"><?php _e('Cart Discount:', 'woocommerce-pip'); ?></th>
-						<td style="text-align:left;"><?php echo wc_price($order->cart_discount); ?></td>
+						<td style="text-align:right;"><?php echo wc_price($order->cart_discount); ?></td>
 					</tr><?php endif; ?>
 					<?php if ($order->order_discount > 0) : ?><tr class="desc" >
 					  <th colspan="2" style="text-align:left; padding-top: 12px;">&nbsp;</th>
 						<th class="info" scope="row" style="text-align:left;"><?php _e('Order Discount:', 'woocommerce-pip'); ?></th>
-						<td style="text-align:left;"><?php echo wc_price($order->order_discount); ?></td>
+						<td style="text-align:right;"><?php echo wc_price($order->order_discount); ?></td>
 					</tr><?php endif; ?>
 					<!-- If there is more than one tax... -->
 					<?php $tax_items = $order->get_tax_totals(); ?>
 					<?php if ( count( $tax_items ) > 1 ) : foreach ( $tax_items as $tax_item ) : ?><tr class="desc" >
 					<th colspan="2" style="text-align:left; padding-top: 12px;">&nbsp;</th>
 						<th class="info" scope="row" style="text-align:left;"><?php echo esc_html( $tax_item->label ); ?>:</th>
-						<td style="text-align:left;"><?php echo wc_price( $tax_item->amount ); ?></td>
+						<td style="text-align:rightt;"><?php echo wc_price( $tax_item->amount ); ?></td>
 					</tr>
 					<?php endforeach; ?><tr class="desc" >
 						<th colspan="2" style="text-align:left; padding-top: 12px;">&nbsp;</th>
 						<th class="info" scope="row" style="text-align:right;"><?php _e( 'Total Tax:', 'woocommerce-pip' ); ?></th>
-						<td style="text-align:left;"><?php echo wc_price( $order->get_total_tax() ); ?></td>
+						<td style="text-align:right;"><?php echo wc_price( $order->get_total_tax() ); ?></td>
 					</tr>
 					<!-- if there is only one tax... -->
 					<?php else: foreach ( $tax_items as $tax_item ): ?><tr class="desc" >
 						<th colspan="2" style="text-align:left; padding-top: 12px;">&nbsp;</th>
 						<th class="info" scope="row" style="text-align:left;"><?php echo esc_html( $tax_item->label ); ?>:</th>
-						<td style="text-align:left;"><?php echo wc_price( $tax_item->amount ); ?></td>
+						<td style="text-align:right;"><?php echo wc_price( $tax_item->amount ); ?></td>
 					</tr><?php endforeach; endif; ?>
 					<!-- end if -->
 					<tr class="desc" >
 					  <th colspan="2" style="text-align:left; padding-top: 12px;">&nbsp;</th>
 						<th class="info" scope="row" style="text-align:left;"><?php _e('Total:', 'woocommerce-pip'); ?></th>
-						<td class="totalprice" style="text-align:left;"><?php echo wc_price($order->order_total); ?></td>
+						<td class="totalprice" style="text-align:right;"><?php echo wc_price($order->order_total); ?></td>
 					</tr>
 				</tfoot>
 				<tbody>
@@ -160,9 +161,9 @@
 				<thead>
 					<tr class="desc">
 					  <th scope="col" style="text-align:left; width: 15%;"><?php _e('SKU', 'woocommerce-pip'); ?></th>
-						<th scope="col" style="text-align:left; width: 45%;"><?php _e('Product', 'woocommerce-pip'); ?></th>
+						<th scope="col" style="text-align:left; width: 60%;"><?php _e('Product', 'woocommerce-pip'); ?></th>
 						<th scope="col" style="text-align:left; width: 10%;"><?php _e('Quantity', 'woocommerce-pip'); ?></th>
-						<th scope="col" style="text-align:left; width: 30%;"><?php _e('Total Weight', 'woocommerce-pip'); ?></th>
+						<th scope="col" style="text-align:left; width: 15%;"><?php _e('Total Weight', 'woocommerce-pip'); ?></th>
 					</tr>
 				</thead>
 				<tbody class="class="item"">
